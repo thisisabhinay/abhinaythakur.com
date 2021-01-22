67419d9c7fafe5d11a3ff07806ce312141d4ef67
Abhinay Thakur <a8h1nay@gmail.com>
2021-01-23 00:48:03 +0530

    Create Portfolio component

---
+++ src/app/data.ts
@@ -0,0 +1,129 @@
+export const resume = {
+    "experience": [{
+        "designation": "Sr. UX/UI Designer",
+        "years": "2019 - Present",
+        "company": "Virtual Diamond Boutique",
+        "location": "Gurugram, India",
+        "description": [{
+            text: "Working as a Sr. UX/UI designer in a diverse multi-cultural team. Major contributions to the organization till date — Apart from being responsible for improving the overall UX/UI design of the product, also handling the client-facing design processes (white labelling, logo designing)."
+        }, {
+            text: "Improving the design workflow within organisation by introducing and maintaining proven methodologies like creation of centralised design system."
+        }, {
+            text: "Integrating design cycle effectively within sprint planning."
+        }, {
+            text: "Automating the hand-off processes between designers and developers, designing the workflow for easy collaboration among designers. Developing in-house tools for the same."
+        }, {
+            text: "Upgrading & maintaining the style guides and design component (icons, colours) libraries."
+        }, {
+            text: "On-going efforts are on inventing a design language based on organisation's core philosophy, very much like Google's material design, Microsoft's fluent design system and many more."
+        }]
+    }, {
+        "designation": "Lead UX/UI Designer & Frontend Developer",
+        "years": "2017 - 2019",
+        "company": "Orgzit",
+        "location": "New Delhi, India",
+        "description": [{
+            text: "Took the ownership of complete UX/UI designing and Frontend engineering cycles within the organisation."
+        }, {
+            text: "Designed, architect and build new web application features like:",
+            description: [{
+                heading: "Global Search",
+                text: "Searches all types of data (record, comments, files, history etc.) throughout the application from one single and globally available input point."
+            }, {
+                heading: "User Management",
+                text: "Searches all types of data (record, comments, files, history etc.) throughout the application from one single and globally available input point."
+            }, {
+                heading: "Dashboard",
+                text: "An entry point in the application where user can visualize different states their project is in and various reports and analysis related to it. Also, the user can have an overview of the discussions (team collaboration) and tasks assigned to him."
+            }, {
+                heading: "Widgets",
+                text: "Infographic widgets (Reports), Single valued widgets, 'My Discussion' widget, 'My Task' widget and 'Projects Shortcut' widget for facilitating the user with information and providing ease of access."
+            }, {
+                heading: "Dataform Views",
+                text: "Different contexts of viewing the same data to get a clear idea about the big picture. The views designed and developed were: Table View, Kanban View, Card View and Map View."
+            }]
+        }, {
+            text: "Upgraded and owned the frontend of product’s web application giving fortnightly/monthly builds."
+        }, {
+            text: "Maintained and fixed various pre-existent features like ​Workflows, Web Socket Integration, Data Filters, Workspace & App Management etc. "
+        }, {
+            text: "Designed and implemented the user experience flow of the on-boarding process. "
+        }, {
+            text: "Managed the development and maintenance of the marketing website of the business."
+        }, {
+            text: "Managed the technical and design team in planning, development and also helped with defining specifications, reviews, user testing etc."
+        }, {
+            text: "Designed (UX/UI) & Implemented core business features and clients requirements on the Orgzit product and therefore in the process catering various use-cases like​ Lead Management, Order Management, CRM, ERP etc."
+        }]
+    }, {
+        "designation": "Lead UX/UI Designer & Developer",
+        "years": "2016 - 2017",
+        "company": "Fanzoo Sports",
+        "location": "New Delhi, India",
+        "description": [{
+            text: "Given responsibility for both - designing and developing - efficient, usable and visually compelling interfaces and experience for the fantasy sports web app. The web app was in PHP stack with CakePHP as a framework and worked on front-end with jQuery and JavaScript."
+        }]
+    }],
+    "education": [{
+        "institute": "Amity University",
+        "years": "2012 - 2016",
+        "course": "B.Tech, Computer Science and Engineering",
+        "location": "Gurugram, Haryana, India",
+    }],
+    "skills": [{
+        "domain": "User Experience",
+        "skills": [
+            "User Research​",
+            "Human-Computer Interaction",
+            "Design Thinking",
+            "Wireframes",
+            "Information Architecture",
+            "Interface Design",
+            "Visual Design",
+            "Lean UX methodologies",
+            "Usability Testing",
+            "Gamification/Game Design"
+        ]
+    }, {
+        "domain": "Visual",
+        "skills": [
+            "Sketching/Drawing",
+            "Illustration",
+            "Image Editing",
+            "Video Editing",
+            "Storyboarding"
+        ]
+    }, {
+        "domain": "Prototyping",
+        "skills": [
+            "HTML/CSS",
+            "JavaScript",
+            "JQuery",
+            "Angualr",
+            "React.js"
+        ]
+    }, {
+        "domain": "Softwares & Tools",
+        "skills": [
+            "Adobe Photoshop",
+            "Adobe XD",
+            "Sketch App",
+            "Adobe Illustrator",
+            "Adobe After Effects",
+            "Figma",
+            "Invision",
+            "Marvel App",
+            "GitHub",
+            "Unity 3D"
+        ]
+    }],
+    "contact": [{
+        title: "Email",
+        description: "sayhello@abhinaythakur.com",
+        social: [
+            "https://www.linkedin.com/in/abhinaythakur/",
+            "https://behance.net/abhinaythakur",
+            "https://dribbble.com/abhinaythakur"
+        ]
+    }]
+};
