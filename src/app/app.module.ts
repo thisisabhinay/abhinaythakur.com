1dbfabf4c461c0a5ff401c43fb16391cd225baf3
Abhinay Thakur <a8h1nay@gmail.com>
2021-01-23 20:21:22 +0530

    Remove List Skills component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,13 @@ import { LayoutMainComponent } from "./layout/layout-main/layout-main.component"
 import { NavbarComponent } from "./navbar/navbar.component";
 import { HeaderTopComponent } from "./header/header-top/header-top.component";
 import { IntroComponent } from "./intro/intro.component";
-import { HomeComponent } from './page/home/home.component';
-import { LabelValueComponent } from './label-value/label-value.component';
-import { SocialIconsComponent } from './social-icons/social-icons.component';
-import { HeaderSectionComponent } from './header/header-section/header-section.component';
-import { PortfolioComponent } from './page/portfolio/portfolio.component';
-import { CardComponent } from './card/card.component';
-import { ListComponent } from './list/list.component';
-import { ListSkillsComponent } from './list-skills/list-skills.component';
+import { HomeComponent } from "./page/home/home.component";
+import { LabelValueComponent } from "./label-value/label-value.component";
+import { SocialIconsComponent } from "./social-icons/social-icons.component";
+import { HeaderSectionComponent } from "./header/header-section/header-section.component";
+import { PortfolioComponent } from "./page/portfolio/portfolio.component";
+import { CardComponent } from "./card/card.component";
+import { ListComponent } from "./list/list.component";
 
 @NgModule({
     declarations: [
@@ -29,8 +28,7 @@ import { ListSkillsComponent } from './list-skills/list-skills.component';
         HeaderSectionComponent,
         PortfolioComponent,
         CardComponent,
-        ListComponent,
-        ListSkillsComponent
+        ListComponent
     ],
     imports: [
         BrowserModule,
