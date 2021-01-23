1f7cd47ab6fdb8877906c9d482f457103bd78687
Abhinay Thakur <a8h1nay@gmail.com>
2021-01-23 15:03:56 +0530

    Create basic generative art

--- src/app/intro/intro.component.ts
+++ src/app/intro/intro.component.ts
@@ -1,5 +1,6 @@
+/* eslint-disable prefer-const */
 /* eslint-disable @typescript-eslint/no-empty-function */
-import { Component, OnInit } from "@angular/core";
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
 
 @Component({
     selector: "app-intro",
@@ -8,9 +9,21 @@ import { Component, OnInit } from "@angular/core";
 })
 
 export class IntroComponent implements OnInit {
+    ctx: any;
+    @ViewChild("canvas") canvas: ElementRef<HTMLCanvasElement>;
+
     constructor() { }
 
     ngOnInit(): void {
     }
 
+    ngAfterViewChecked(): void {
+        console.log(this.canvas);
+        this.ctx = this.canvas.nativeElement.getContext("2d");
+
+        this.ctx.fillStyle = "#0924FC";
+        this.ctx.fillRect(150, 50, 400, 400);
+        this.ctx.clearRect(200, 200, 200, 200);
+        this.ctx.strokeRect(250, 250, 150, 150);
+    }
 }
