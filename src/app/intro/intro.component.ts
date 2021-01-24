b1a83e376d04d69969b68987950fcbd8cb3d6a2e
Abhinay Thakur <a8h1nay@gmail.com>
2021-01-24 19:16:52 +0530

    Add abstract video to show on landing page

--- src/app/intro/intro.component.ts
+++ src/app/intro/intro.component.ts
@@ -1,6 +1,6 @@
 /* eslint-disable prefer-const */
 /* eslint-disable @typescript-eslint/no-empty-function */
-import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
     selector: "app-intro",
@@ -9,21 +9,8 @@ import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
 })
 
 export class IntroComponent implements OnInit {
-    ctx: any;
-    @ViewChild("canvas") canvas: ElementRef<HTMLCanvasElement>;
-
     constructor() { }
 
     ngOnInit(): void {
-    }
-
-    ngAfterViewChecked(): void {
-        console.log(this.canvas);
-        this.ctx = this.canvas.nativeElement.getContext("2d");
-
-        this.ctx.fillStyle = "#f1f4f7";
-        this.ctx.fillRect(150, 50, 400, 400);
-        this.ctx.clearRect(200, 200, 200, 200);
-        this.ctx.strokeRect(250, 250, 150, 150);
     }
 }
