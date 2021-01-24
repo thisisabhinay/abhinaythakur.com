aa0e373799ff4ce3d842949ba13e5fb9c3128410
Abhinay Thakur <a8h1nay@gmail.com>
2021-01-24 15:57:04 +0530

    Add random query for sample images in portfolio data model

--- src/app/portfolio.ts
+++ src/app/portfolio.ts
@@ -5,7 +5,7 @@ export const portfolio = [{
         title: "Comparison Engine",
         description: "A case study in applying lean ux to bring an idea into market.",
         url: "#",
-        cover: "https://picsum.photos/400"
+        cover: "https://loremflickr.com/400/400/all?random=0"
     }]
 }, {
     section: "Design Thinking",
@@ -14,37 +14,37 @@ export const portfolio = [{
         title: "Socialization of online fantasy sports",
         description: "Designing the social engagement experience of a fantasy sports platform on web.",
         url: "#",
-        cover: "https://picsum.photos/400"
+        cover: "https://loremflickr.com/400/400/all?random=1"
     }, {
         title: "Improving workflow between teams at VDB",
         description: "Using design thinking to come up with better tools for automating the tasks and processes at VDB.",
         url: "#",
-        cover: "https://picsum.photos/400"
+        cover: "https://loremflickr.com/400/400/all?random=2"
     }, {
         title: "Presenting multifaceted data at Orgzit",
         description: "Designing a better solution to display same set of data through different perspectives as per user's need.",
         url: "#",
-        cover: "https://picsum.photos/400"
+        cover: "https://loremflickr.com/400/400/all?random=3"
     }, {
         title: "Orgzit Records",
         description: "Improving interactions related to data records and their relations.",
         url: "#",
-        cover: "https://picsum.photos/400"
+        cover: "https://loremflickr.com/400/400/all?random=4"
     }, {
         title: "Orgzit Teams",
         description: "Designing the collaboration and communication experience.",
         url: "#",
-        cover: "https://picsum.photos/400"
+        cover: "https://loremflickr.com/400/400/all?random=5"
     }, {
         title: "VDB Shopping",
         description: "Designing the B2C Ecommerce experience.",
         url: "#",
-        cover: "https://picsum.photos/400"
+        cover: "https://loremflickr.com/400/400/all?random=6"
     }, {
         title: "VDB Bulk Request",
         description: "Designing the B2B Ecommerce experience.",
         url: "#",
-        cover: "https://picsum.photos/400"
+        cover: "https://loremflickr.com/400/400/all?random=7"
     }]
 }, {
     section: "Gamification of UX",
@@ -53,17 +53,17 @@ export const portfolio = [{
         title: "Gamifying Fantasy Leagues",
         description: "Making leauge participation a fun activity.",
         url: "#",
-        cover: "https://picsum.photos/400"
+        cover: "https://loremflickr.com/400/400/all?random=8"
     }, {
         title: "Gamification of Team Collaboration",
         description: "Designing incentive for team participation at Orgzit.",
         url: "#",
-        cover: "https://picsum.photos/400"
+        cover: "https://loremflickr.com/400/400/all?random=9"
     }, {
         title: "Zombiecalypse",
         description: "Using user-interface as a medium of storytelling and world building.",
         url: "#",
-        cover: "https://picsum.photos/400"
+        cover: "https://loremflickr.com/400/400/all?random=10"
     }]
 }, {
     section: "Game Design",
@@ -72,12 +72,12 @@ export const portfolio = [{
         title: "Last Man",
         description: "A post apocalyptic sci-fi narrative expressed through puzzle based gameplay mechanics.",
         url: "#",
-        cover: "https://picsum.photos/400"
+        cover: "https://loremflickr.com/400/400/all?random=11"
     }, {
         title: "The Second Moon",
         description: "A short visual novel game.",
         url: "#",
-        cover: "https://picsum.photos/400"
+        cover: "https://loremflickr.com/400/400/all?random=12"
     }]
 }, {
     section: "Visual Design",
@@ -86,12 +86,12 @@ export const portfolio = [{
         title: "Ritz",
         description: "A grandeur and ostentatious design language.",
         url: "#",
-        cover: "https://picsum.photos/400"
+        cover: "https://loremflickr.com/400/400/all?random=13"
     }, {
         title: "Abhinay Thakur",
         description: "A generative art expressing the personality.",
         url: "#",
-        cover: "https://picsum.photos/400"
+        cover: "https://loremflickr.com/400/400/all?random=14"
     }]
 }, {
     section: "Prototyping",
@@ -100,22 +100,22 @@ export const portfolio = [{
         title: "Fanzoo Leaderboard",
         description: "Implementing the Live Leaderboard in native JS.",
         url: "#",
-        cover: "https://picsum.photos/400"
+        cover: "https://loremflickr.com/400/400/all?random=15"
     }, {
         title: "Fanzoo Teams",
         description: "Implementing Team Selection view in native JS.",
         url: "#",
-        cover: "https://picsum.photos/400"
+        cover: "https://loremflickr.com/400/400/all?random=16"
     }, {
         title: "Orgzit Forms",
         description: "Implementing the dataform views in Backbone.js",
         url: "#",
-        cover: "https://picsum.photos/400"
+        cover: "https://loremflickr.com/400/400/all?random=17"
     }, {
         title: "VDB Shared Collection View",
         description: "An angular implementation.",
         url: "#",
-        cover: "https://picsum.photos/400"
+        cover: "https://loremflickr.com/400/400/all?random=18"
     }]
 }, {
     section: "Ideation / Concept Design",
@@ -124,26 +124,26 @@ export const portfolio = [{
         title: "Gamification of Team Collaboration",
         description: "Storyboard and concept sketches.",
         url: "#",
-        cover: "https://picsum.photos/400"
+        cover: "https://loremflickr.com/400/400/all?random=19"
     }, {
         title: "Last Man",
         description: "Concept Art.",
         url: "#",
-        cover: "https://picsum.photos/400"
+        cover: "https://loremflickr.com/400/400/all?random=20"
     }, {
         title: "Fanzoo Gamification",
         description: "Storyboard and concept sketches.",
         url: "#",
-        cover: "https://picsum.photos/400"
+        cover: "https://loremflickr.com/400/400/all?random=21"
     }, {
         title: "The Second Moon",
         description: "Concept Art.",
         url: "#",
-        cover: "https://picsum.photos/400"
+        cover: "https://loremflickr.com/400/400/all?random=22"
     }, {
         title: "Zombiecalypse",
         description: "Ideation and concept sketches.",
         url: "#",
-        cover: "https://picsum.photos/400"
+        cover: "https://loremflickr.com/400/400/all?random=23"
     }]
 }];
